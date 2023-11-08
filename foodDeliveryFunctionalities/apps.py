a32782ebca1fe45a4ab286ed766d1fca187e3d55
nihar <nihar@marolix.com>
2023-11-08 11:34:51 +0530

    food Delivery application generic django template

---
+++ foodDeliveryFunctionalities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FooddeliveryfunctionalitiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'foodDeliveryFunctionalities'
