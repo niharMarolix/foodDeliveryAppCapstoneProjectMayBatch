745e02938049742aa8012955e216ae12bfc00a8c
sandhya <sandhya19.marolix@gmail.com>
2023-11-09 13:00:39 +0530

    registration completed

--- foodDeliveryFunctionalities/models.py
+++ foodDeliveryFunctionalities/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    address=models.TextField()
+    isRestaurantOwner=models.BooleanField(default=False)
+    isDeliveryPatner=models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.username
